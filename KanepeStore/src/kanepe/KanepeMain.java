6a72ba6d9057df5299c27ce3f5f6fed41ae0db59
Vinícius <vinicius.pn@aluno.ifsc.edu.br>
2023-09-27 21:53:25 -0300

    Criação do método login e loginADM
    adicionado os dois métodos e implementados no switch , poucas alterações em outros métodos

--- KanepeStore/src/kanepe/KanepeMain.java
+++ KanepeStore/src/kanepe/KanepeMain.java
@@ -5,73 +5,149 @@ import java.util.Scanner;
 
 public class KanepeMain {
 
+	private static final String NOMEADM = "LoginADM";
+	private static final String SENHAADM = "SenhaADM";	
+	
 	public static void main(String[] args) {
-		ArrayList<Usuario> novoCadastro = new ArrayList<>(); 
+		ArrayList<Usuario> novoCadastro = new ArrayList<>();
 		Scanner leitura = new Scanner(System.in);
 		Integer opcao = Integer.MAX_VALUE;
-		
+
 		while (opcao != 4) {
 			opcao = menu1();
-			
+
 			switch (opcao) {
 			case 1: {
 				novoCadastro.add(cadastro());
 			}
-			break;
-			case 4: {
-				opcao=4;
+				break;
+			case 2: {
+				if (loginADM()) {
+                    System.out.println("Login ADM bem-sucedido!");
+                } else {
+                    System.out.println("Login ADM falhou.");
+                }
 			}
-			break;
+				break;
+			case 3: {
+				Usuario usuarioLogado = login(novoCadastro);
+				if (usuarioLogado != null) {
+					System.out.println("Login bem-sucedido para: " + usuarioLogado.getNome());
+				} else {
+					System.out.println("Nome de usuário ou senha incorretos.");
+				}
+
+			}
+				break;
+			case 4: {
+				System.out.println("Programa encerrado!");
+				opcao = 4;
+			}
+				break;
 			}
 
 		}
 
 	}
-//	===================================MENUS======================================================================
+//	----------------------------------MENUS-----------------------------------------------------------------------
+
 	
 //	===================================MENUPRINCIPAL==============================================================
 	private static Integer menu1() {
-		
+
 		System.out.println("BEM VINDO A KANEPE\n");
 		System.out.println("1 - Cadastrar");
 		System.out.println("2 - LogarADM");
 		System.out.println("3 - Logar");
 		System.out.println("4 - Sair\n");
 		System.out.println("selecione a opção para continuar:");
-		
+
 		Scanner leitura = new Scanner(System.in);
-		
+
 		Integer opcao = Integer.valueOf(leitura.nextLine());
-		
-		leitura.close();
+
 		return opcao;
-		
+
 	}
-//	====================================MENUCADASTRO================================================================
-	
+//	====================================CADASTRO================================================================
+
 	private static Usuario cadastro() {
 		Usuario pessoa = new Usuario();
 		Scanner leitura = new Scanner(System.in);
-		
-		Integer lup = 1;
-		
-		while(lup!=0) {
-		System.out.println("Digite o nome(8 ou mais caracteres)");
-		String nome = leitura.nextLine();
-		System.out.println("Digite a senha(8 ou mais caracteres)");
-		String senha = leitura.nextLine();
-		if(nome.length()>=8&&senha.length()>=8) {
-			pessoa.setNome(nome);
-			pessoa.setSenha(senha);
-			
-			System.out.println("Cadastro realizado com sucesso!");
-			lup = 0;
-			
-		}else {
-			System.out.println("Cadastro invalido!\n");
-		}
+
+		boolean cadastroValido = false;
+
+		while (!cadastroValido) {
+			System.out.println("Digite o nome (8 ou mais caracteres)");
+			String nome = leitura.nextLine();
+			System.out.println("Digite a senha (8 ou mais caracteres)");
+			String senha = leitura.nextLine();
+
+			if (nome.length() >= 8 && senha.length() >= 8) {
+				pessoa.setNome(nome);
+				pessoa.setSenha(senha);
+
+				System.out.println("Cadastro realizado com sucesso!");
+				cadastroValido = true;
+			} else {
+				System.out.println("Cadastro inválido!\n");
+
+				System.out.println("Deseja tentar novamente? (S/N)");
+				String resposta = leitura.nextLine();
+
+				if (!resposta.equalsIgnoreCase("S")) {
+					break;
+				}
+			}
 		}
 		return pessoa;
 	}
 
+//	====================================login================================================================
+
+	private static Usuario login(ArrayList<Usuario> novoCadastro) {
+		Usuario pessoa = new Usuario();
+		Scanner leitura = new Scanner(System.in);
+
+		boolean loginValido = false;
+
+		while (!loginValido) {
+			System.out.println("Digite o nome de usuário:");
+			String nomeUsuario = leitura.nextLine();
+			System.out.println("Digite a senha:");
+			String senha = leitura.nextLine();
+
+			for (Usuario usuario : novoCadastro) {
+				if (usuario.getNome().equals(nomeUsuario) && usuario.getSenha().equals(senha)) {
+					loginValido = true;
+					pessoa = usuario;
+					break;
+				}
+			}
+
+			if (!loginValido) {
+				System.out.println("Nome de usuário ou senha incorretos. Deseja tentar novamente? (S/N)");
+				String resposta = leitura.nextLine();
+
+				if (!resposta.equalsIgnoreCase("S")) {
+					break;
+				}
+			}
+		}
+		return loginValido ? pessoa : null;
+	}
+
+//	====================================LoginADM================================================================
+	
+	private static boolean loginADM() {
+	    Scanner leitura = new Scanner(System.in);
+	    
+	    System.out.println("Digite o nome de usuário ADM:");
+	    String nomeUsuarioADM = leitura.nextLine();
+	    System.out.println("Digite a senha ADM:");
+	    String senhaADM = leitura.nextLine();
+	    
+	    return nomeUsuarioADM.equals(NOMEADM) && senhaADM.equals(SENHAADM);
+	}
+	
 }
