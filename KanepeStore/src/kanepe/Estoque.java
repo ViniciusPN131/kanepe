fa68f3da1968362b2233260abaf97f067eb3073c
Benjamim Schmitt Barreto <benjamim.sb03@aluno.ifsc.edu.br>
2023-09-28 15:13:26 -0300

    Criação tela ADM

---
+++ KanepeStore/src/kanepe/Estoque.java
@@ -0,0 +1,51 @@
+package kanepe;
+
+public class Estoque {
+
+	private String nome;
+	private String especie;
+	private Integer id;
+	private String validade;
+	private Double preco;
+	private String tipoProduto;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getValidade() {
+		return validade;
+	}
+	public void setValidade(String validade) {
+		this.validade = validade;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getTipoProduto() {
+		return tipoProduto;
+	}
+	public void setTipoProduto(String tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+	
+	
+	
+}
--- KanepeStore/src/kanepe/KanepeMain.java
+++ KanepeStore/src/kanepe/KanepeMain.java
@@ -10,6 +10,7 @@ public class KanepeMain {
 
 	public static void main(String[] args) {
 		ArrayList<Usuario> novoCadastro = new ArrayList<>();
+
 		Scanner leitura = new Scanner(System.in);
 		Integer opcao = Integer.MAX_VALUE;
 
@@ -24,10 +25,14 @@ public class KanepeMain {
 			case 2: {
 				if (loginADM()) {
 					System.out.println("Login ADM bem-sucedido!");
+					menuADM();
+
 				} else {
 					System.out.println("Login ADM falhou.");
 				}
+
 			}
+
 				break;
 			case 3: {
 				Usuario usuarioLogado = login(novoCadastro);
@@ -49,6 +54,38 @@ public class KanepeMain {
 		}
 
 	}
+
+//	====================================MenuADM================================================================
+	private static Integer menuADM() {
+		Integer opcao = 0;
+		Scanner leitura = new Scanner(System.in);
+		ArrayList<Estoque> listaItens = new ArrayList<>();
+
+		while (opcao != 4) {
+			System.out.println("BEM VINDO A KANEPE\n");
+			System.out.println("0 - Adcionar item");
+			System.out.println("1 - Remover item");
+			System.out.println("2 - Alteral item");
+			System.out.println("3 - Listar item");
+			System.out.println("4 - Sair\n");
+			System.out.println("selecione a opção para continuar:");
+
+			opcao = Integer.valueOf(leitura.nextLine());
+
+			switch (opcao) {
+			case 0: {
+				listaItens.add(adicionarItem());
+
+			}
+				break;
+			}
+
+		}
+		return opcao;
+	}
+
+//	====================================MenuUsuario================================================================
+
 //	----------------------------------MENUS-----------------------------------------------------------------------
 
 //	===================================MENUPRINCIPAL==============================================================
@@ -139,35 +176,64 @@ public class KanepeMain {
 //	====================================LoginADM================================================================
 
 	private static boolean loginADM() {
-	    Scanner leitura = new Scanner(System.in);
-	    String nomeUsuarioADM = "";
-	    String senhaADM = "";
+		Scanner leitura = new Scanner(System.in);
+		String nomeUsuarioADM = "";
+		String senhaADM = "";
 
 		boolean loginValido = false;
 
 		while (!loginValido) {
-	    System.out.println("Digite o nome de usuário ADM:");
-	    nomeUsuarioADM = leitura.nextLine();
-	    System.out.println("Digite a senha ADM:");
-	    senhaADM = leitura.nextLine();
-	    
-	    if(nomeUsuarioADM.equals(NOMEADM)&&senhaADM.equals(SENHAADM)) {
-	    	loginValido = true;
-	    	break;
-	    }
-	    
-	    if (!loginValido) {
-			System.out.println("Nome de usuário ou senha incorretos. Deseja tentar novamente? (S/N)");
-			String resposta = leitura.nextLine();
+			System.out.println("Digite o nome de usuário ADM:");
+			nomeUsuarioADM = leitura.nextLine();
+			System.out.println("Digite a senha ADM:");
+			senhaADM = leitura.nextLine();
 
-			if (!resposta.equalsIgnoreCase("S")) {
+			if (nomeUsuarioADM.equals(NOMEADM) && senhaADM.equals(SENHAADM)) {
+				loginValido = true;
 				break;
 			}
+
+			if (!loginValido) {
+				System.out.println("Nome de usuário ou senha incorretos. Deseja tentar novamente? (S/N)");
+				String resposta = leitura.nextLine();
+
+				if (!resposta.equalsIgnoreCase("S")) {
+					break;
+				}
+			}
+
 		}
-	    
-		}
-	    
-		return loginValido ? true : false;
+
+		return loginValido ? true : null;
 	}
 
+// =================================================Adcionar===============================================================
+	private static Estoque adicionarItem() {
+		Scanner leitura = new Scanner(System.in);
+		Estoque item = new Estoque();
+
+		System.out.println("Digite o nome ");
+		String nome = leitura.nextLine();
+		System.out.println("Digite a especie");
+		String especie = leitura.nextLine();
+		System.out.println("Digite o id");
+		Integer id = Integer.valueOf(leitura.nextLine());
+		System.out.println("Digite a validade");
+		String validade = leitura.nextLine();
+		System.out.println("Digite o preco");
+		Double opcao = Double.valueOf(leitura.nextLine());
+		System.out.println("Digite o tipo");
+		String tipoProtudo = leitura.nextLine();
+
+		item.setNome(nome);
+		item.setEspecie(especie);
+		item.setId(id);
+		item.setValidade(validade);
+		item.setPreco(opcao);
+		item.setTipoProduto(tipoProtudo);
+		
+		System.out.println("Adcioando com Sucesso");
+
+		return item;
+	}
 }
